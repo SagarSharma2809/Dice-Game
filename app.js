960ced0c60dfc11cc99ce6024e11eeb59621d29a
Sagar Sharma <sagarsharma2809@gmail.com>
2023-05-27 00:34:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let message = " ";
+
+let leftDice = document.querySelector(".img1");
+leftDice.setAttribute("src", `images/dice${randomNumber1}.png`);
+
+let rightDice = document.querySelector(".img2");
+rightDice.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+    message = "Player 1 Wins! 🚩";
+}
+else if (randomNumber1 < randomNumber2) {
+    message = "🚩 Player 2 Wins!";
+}
+else {
+    message = "Draw";
+}
+
+let heading = document.querySelector("h1");
+heading.textContent = message;
