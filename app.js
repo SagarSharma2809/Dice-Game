97a2abce53062fcba0e6ac7cbb8075b49a22098f
Sagar Sharma <sagarsharma2809@gmail.com>
2023-05-27 00:39:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let message = " ";
 
 let leftDice = document.querySelector(".img1");
-leftDice.setAttribute("src", `images/dice${randomNumber1}.png`);
+leftDice.setAttribute("src", `dice${randomNumber1}.png`);
 
 let rightDice = document.querySelector(".img2");
-rightDice.setAttribute("src", `images/dice${randomNumber2}.png`);
+rightDice.setAttribute("src", `dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2) {
     message = "Player 1 Wins! 🚩";
